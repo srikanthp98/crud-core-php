db842ad19b87418b19f17e25bced0f7f5a8f91f1
srikanthp98 <sripallerla498@gmail.com>
2019-11-06 13:20:01 +0530

    core-php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'user@123');
+define('DB_NAME', 'srikanth');
+ 
+/* Attempt to connect to MySQL database */
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($mysqli === false){
+  die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+?>
